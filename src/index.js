61766fe57ba8d52607412229ec4babcaba1d4907
har2525life <har2525life@outlook.com>
2023-06-28 00:10:00 +0900

    add: earth model

--- src/index.js
+++ src/index.js
@@ -5,9 +5,13 @@ import {
   BoxGeometry,
   MeshNormalMaterial,
   Mesh,
+  AmbientLight,
 } from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls";
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader';
 
+import ISS from "./images/ISS.glb"
+import earth from "./images/earth.glb"
 import "./styles.css";
 
 // sizes
@@ -19,18 +23,37 @@ const sizes = {
 // canvas
 const canvas = document.getElementById("canvas");
 
+// glb loader
+const loader = new GLTFLoader();
+
 // Scene
 const scene = new Scene();
 
-const camera = new PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 1000);
-camera.position.set(0,0,10)
+const camera = new PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100000);
+camera.position.set(0,0,1000)
 scene.add(camera);
 
-// geometry
-const boxGeometry = new BoxGeometry(1, 1, 1);
-const boxMaterial = new MeshNormalMaterial();
-const box = new Mesh(boxGeometry, boxMaterial);
-scene.add(box);
+// // geometry
+// const boxGeometry = new BoxGeometry(1, 1, 1);
+// const boxMaterial = new MeshNormalMaterial();
+// const box = new Mesh(boxGeometry, boxMaterial);
+// scene.add(box);
+
+// load a glb file
+
+loader.load(earth, (glb) => {
+    console.log(glb)
+    scene.add(glb.scene);
+})
+loader.load(ISS, (glb) => {
+    console.log(glb)
+    const iss = glb.scene
+    
+    scene.add(glb.scene);
+})
+
+const light = new AmbientLight(0xffffff)
+scene.add(light)
 
 // control
 const controls = new OrbitControls(camera, canvas);
