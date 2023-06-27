2f1ed49580f5e16658cf174bc614f517a407427e
har2525life <har2525life@outlook.com>
2023-06-27 23:15:49 +0900

    add: environment

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+module.exports = {
+  // 開発環境(development) or 本番環境(production) ?
+  mode: "development",
+  // どのファイルを最初に見に行くのか コンパイル時
+  entry: "./src/index.js",
+  // 出力先 __dirname 現在のwebpackの階層と同じ
+  output: {
+    path: `${__dirname}/dist`,
+    filename: "bundle.js",
+  },
+  // ローカルサーバーで見に行くときの設定
+  devServer: {
+    static: "./dist",
+  },
+  // 拡張子を書かなくてもよくするように
+  resolve: {
+    extensions: [".js", ".glsl"],
+  },
+  // コンパイラの設定
+  module: {
+    rules: [
+      // javascript
+      {
+        // .jsファイルを古い記述にコンパイルしてね
+        test: /\.js$/,
+        // node_modulesはコンパイルしなくてもいいよ
+        exclude: /node_modules/,
+        // コンパイルにはbabel-loaderを使用するよ
+        use: ["babel-loader"],
+      },
+      //  Shader
+      {
+        test: /\.(glsl|vs|fs|vert|frag)$/,
+        type: "asset/source",
+        generator: {
+          filename: "assets/images/[hash][ext]",
+        },
+      },
+      //   images
+      {
+        test: /\.(jpg|png|gif|svg)$/,
+        type: "asset/resource",
+        generator: {
+          filename: "assets/images/[name][ext]",
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
